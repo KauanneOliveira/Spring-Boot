d1a498841d02884c051d12d9c68301d1beedd39a
Kauanne Oliveira <paulakauanne2411@gmail.com>
2023-04-26 08:26:54 -0300

    atualizando as anotações das classes

--- src/main/java/springboot2/controller/AnimeController.java
+++ src/main/java/springboot2/controller/AnimeController.java
@@ -1,18 +1,30 @@
 package springboot2.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springboot2.domain.Anime;
+import springboot2.util.DateUtil;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
+//@AllArgsConstructor - essa anotação do lombok cria o método contrutor com todos atributos que tem, sem precisar coloca-lo no código
+@RequiredArgsConstructor //tbm vai criar um contrutor com os campos que são finais ( public final ...)
 public class AnimeController {
+
+    //@Autowired injeção de dependência via campos não é recomendado, o recomendado é usar no contrutor
+    public final DateUtil dateUtil;
+
     @GetMapping(path = "list")
     //este método vai retornar uma lista de anime
     public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of( new Anime("DBZ"), new Anime("Berserk"));
     }
 
