e5e0811c747852acd2408ee91defeb717508a102
Kauanne Oliveira <paulakauanne2411@gmail.com>
2023-04-30 12:02:38 -0300

    adicionando paginação

--- src/main/java/br/com/codelab/springboot2/controller/AnimeController.java
+++ src/main/java/br/com/codelab/springboot2/controller/AnimeController.java
@@ -8,6 +8,8 @@ import br.com.codelab.springboot2.service.AnimeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,10 +31,9 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public ResponseEntity<List<Anime>> list(){
-        //este método vai retornar uma lista de anime
+    public ResponseEntity<Page<Anime>> list(Pageable pageable){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return ResponseEntity.ok( animeService.listAll() ); //o ResponseEntity vai retornar informações extras, como o status dessa requisição
+        return ResponseEntity.ok( animeService.listAll(pageable) ); //o ResponseEntity vai retornar informações extras, como o status dessa requisição
     }
 
     @GetMapping(path = "/{id}")
--- src/main/java/br/com/codelab/springboot2/service/AnimeService.java
+++ src/main/java/br/com/codelab/springboot2/service/AnimeService.java
@@ -9,6 +9,8 @@ import br.com.codelab.springboot2.repository.AnimeRepository;
 import br.com.codelab.springboot2.requests.AnimePostRequestBody;
 import br.com.codelab.springboot2.requests.AnimePutRequestBody;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,8 +24,8 @@ public class AnimeService {
 
     private final AnimeRepository animeRepository;
 
-    public List<Anime> listAll(){
-        return animeRepository.findAll();
+    public Page<Anime> listAll(Pageable pageable){
+        return animeRepository.findAll(pageable);
     }
 
     public List<Anime> findByName(String name){
